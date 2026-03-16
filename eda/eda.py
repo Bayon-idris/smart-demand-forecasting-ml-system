5607a5cf564018d7d687832e7c29c9bf8d38bac6
Bayon-idris <bayonidris@gmail.com>
2026-03-17 02:00:39 +0800

    Update EDA Pipeline : Delete unused columns in final data csv file and change date format to yyyy-mm-dd

--- eda/eda.py
+++ eda/eda.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from utils import constant
 
+
 def load_group_best_sales(file_path, chunksize=1_000_000):
     store_sales = defaultdict(int)
     store_products = defaultdict(set)
@@ -19,17 +20,22 @@ def load_group_best_sales(file_path, chunksize=1_000_000):
     summary = []
 
     for store in store_sales:
-        summary.append({
-            "store_id": store,
-            "total_sales": store_sales[store],
-            "unique_products": len(store_products[store])
-        })
+        summary.append(
+            {
+                "store_id": store,
+                "total_sales": store_sales[store],
+                "unique_products": len(store_products[store]),
+            }
+        )
 
     df_summary = pd.DataFrame(summary).sort_values("total_sales", ascending=False)
 
     return df_summary
 
-def create_new_csv_file_based_on_store_best_sales(store_to_extract_id, output_file_path, file_path):
+
+def create_new_csv_file_based_on_store_best_sales(
+    store_to_extract_id, output_file_path, file_path, features
+):
 
     reader = pd.read_csv(file_path, chunksize=1_000_000)
     first_write = True
@@ -45,7 +51,13 @@ def create_new_csv_file_based_on_store_best_sales(store_to_extract_id, output_fi
 
             chunk_store = create_time_series_features(chunk_store)
 
+            chunk_store = add_peak_flag(chunk_store)
+
+            chunk_store = convert_date_in_date_format(chunk_store)
+
             chunk_store = chunk_store.dropna()
+
+            chunk_store = keep_only_features(chunk_store, features)
 
             if first_write:
                 chunk_store.to_csv(f_out, index=False)
@@ -54,21 +66,43 @@ def create_new_csv_file_based_on_store_best_sales(store_to_extract_id, output_fi
                 chunk_store.to_csv(f_out, index=False, header=False)
 
     print("Extraction terminée.")
-    
+
+
 def create_time_series_features(df):
-    """
-    Create additional lag and rolling features for demand forecasting
-    """
 
     # LAGS
-    df["lag_14"] = df["sales"].shift(14)
-    df["lag_28"] = df["sales"].shift(28)
+    df["lag_14"] = df.groupby("item_number")["sales"].shift(14)
+    df["lag_28"] = df.groupby("item_number")["sales"].shift(28)
 
     # ROLLING MEAN
-    df["rolling_mean_7"] = df["sales"].shift(1).rolling(window=7).mean()
-    df["rolling_mean_30"] = df["sales"].shift(1).rolling(window=30).mean()
+    df["rolling_mean_7"] = (
+        df.groupby("item_number")["sales"].shift(1).rolling(window=7).mean()
+    )
+
+    df["rolling_mean_30"] = (
+        df.groupby("item_number")["sales"].shift(1).rolling(window=30).mean()
+    )
 
     # ROLLING STD
-    df["rolling_std_7"] = df["sales"].shift(1).rolling(window=7).std()
+    df["rolling_std_7"] = (
+        df.groupby("item_number")["sales"].shift(1).rolling(window=7).std()
+    )
 
-    return df    
+    return df
+
+
+def add_peak_flag(df):
+
+    df["is_peak"] = (
+        df["sales"] > (df["rolling_mean_30"] + 2 * df["rolling_std_7"])
+    ).astype(int)
+
+    return df
+
+def keep_only_features(df, features):
+    existing_features = [col for col in features if col in df.columns]
+    return df[existing_features]
+
+def convert_date_in_date_format(df):
+    df["date"] = pd.to_datetime(dict(year=df["year"], month=df["month"], day=df["day"]))
+    return df
--- utils/constant.py
+++ utils/constant.py
@@ -1,2 +1,27 @@
 model_base_path = "models/xgboost_model.pkl"
 store_base_path = "data/store_sales.csv"
+
+
+features = [
+    "sales",
+    "date",
+    "wm_yr_wk",
+    "wday",
+    "snap",
+    "lag_1",
+    "lag_7",
+    "lag_14",
+    "lag_28",
+    "rolling_mean_7",
+    "rolling_mean_30",
+    "rolling_std_7",
+    "item_subcategory",
+    "item_number",
+    "sell_price",
+    "price_flag",
+    "snap_weekend",
+    "event_count",
+    "event_impact"
+]
+
+target = "sales"
