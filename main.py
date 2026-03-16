612b4b3f0e71378765d67c9fe63b36072ce7db3a
Bayon-idris <bayonidris@gmail.com>
2026-03-17 05:31:08 +0800

    Switching the dataset as the previous one was too irregular, with many zero-sales days and rare spikes.Improving model reliability. EDA and feature engineering will need adjustments to align with this dataset.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from collections import defaultdict
 from eda.eda import create_new_csv_file_based_on_store_best_sales, create_time_series_features, load_group_best_sales
 from predictions.prediction import predict, predict
+from utils import constant
 from utils.verify_path import verify_data_path
 
 
@@ -9,16 +10,16 @@ file_path = "data/data.csv"
 output_file_path = "data/store_sales.csv"
 def main():
     
-    # verify_data_path(file_path)
-    # reader = pd.read_csv(file_path)
+    verify_data_path(file_path)
+    reader = pd.read_csv(file_path)
     
     # thoses  function are called once time to get the best sales per store and create a new csv file with the best sales of the store CA_3 in this case
     # best_sales_df = load_group_best_sales(file_path) 
-    # create_new_csv_file_based_on_store_best_sales(store_to_extract_id="CA_3", output_file_path=output_file_path, file_path=file_path)
+    create_new_csv_file_based_on_store_best_sales(store_to_extract_id="CA_3", output_file_path=output_file_path, file_path=file_path ,features=constant.features)
 
-    verify_data_path(output_file_path)
-    df = pd.read_csv(output_file_path)
-    predict(output_file_path)
+    # verify_data_path(output_file_path)
+    # df = pd.read_csv(output_file_path)
+    # predict(output_file_path)
 
     
 
--- training/train_model.py
+++ training/train_model.py
@@ -7,41 +7,16 @@ from utils import constant
 
 
 
-features = [
-    "wm_yr_wk",
-    "wday",
-    "snap",
-    "year",
-    "month",
-    "day",
-    "lag_1",
-    "lag_7",
-    "lag_14",
-    "lag_28",
-    "rolling_mean_7",
-    "rolling_mean_30",
-    "rolling_std_7",
-    "item_subcategory",
-    "item_number",
-    "sell_price",
-    "price_flag",
-    "snap_weekend",
-    "event_count",
-    "event_impact"
-]
-
-target = "sales"
 def train(filepath):
 
     df = pd.read_csv(filepath)
 
     train_df = df[df["year"] <= 2014]
 
-    X_train = train_df[features]
-    y_train = train_df[target]
+    X_train = train_df[constant.features]
+    y_train = train_df[constant.target]
 
-    
-    #Find the best parameters with RandomizedSearchCV
+    # Find the best parameters with RandomizedSearchCV
     # model = XGBRegressor(random_state=42)
     # param_dist = {
     #     "n_estimators": [100, 200, 300, 400],
@@ -66,7 +41,7 @@ def train(filepath):
     # random_search.fit(X_train, y_train)
     # print("Best parameters:", random_search.best_params_)
 
-    #Feature importance finding
+    # Feature importance finding
 
     # importance = model.feature_importances_
 
@@ -75,7 +50,7 @@ def train(filepath):
     # ).sort_values(by="importance", ascending=False)
 
     # print(feature_importance)
-    
+
     # Best Paramters found with RandomizedSearchCV by scikit-learn
     model = XGBRegressor(
         learning_rate=0.1,
@@ -84,6 +59,7 @@ def train(filepath):
         subsample=1.0,
         colsample_bytree=1.0,
         random_state=42,
+        objective="reg:pseudohubererror",
     )
     model.fit(X_train, y_train)
 
